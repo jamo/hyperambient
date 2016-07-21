b02358d7d0f9e0c528054d59b8b3efa178d45a72
Jarmo Isotalo <jamo@isotalo.fi>
2016-07-21 16:07:13 +0300

    Code style

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 const light = require('hyperterm-solarized-light')
 const dark = require('hyperterm-solarized-dark')
@@ -6,18 +6,18 @@ const ambient = require('ambientlight')
 
 const threshold = 15000000
 exports.decorateConfig = (config) => {
-  var theme
-  if (lux > threshold ) {
+  const lux = ambient()
+  if (lux > threshold) {
     return light.decorateConfig(config)
   }
   return dark.decorateConfig(config)
 }
 
-var ambient_ctr = 0
+var ambientCounter = 0
 exports.middleware = (store) => (next) => (action) => {
-  ambient_ctr += 1
-  if (ambient_ctr > 100) {
-    ambient_ctr = 0
+  ambientCounter += 1
+  if (ambientCounter > 100) {
+    ambientCounter = 0
     store.dispatch({
       type: 'UPDATE_THEME',
       config: config.getConfig()
@@ -32,13 +32,12 @@ exports.reduceUI = (state, action) => {
       console.log(`Ambient light: ${ambient()}`)
       const lux = ambient()
       var theme
-      if (lux > threshold ) {
+      if (lux > threshold) {
         theme = light.decorateConfig(action.config)
       } else {
-        theme =  dark.decorateConfig(action.config)
+        theme = dark.decorateConfig(action.config)
       }
 
-      const config = action.payload
       return state.set('foregroundColor', theme.foregroundColor)
                   .set('backgroundColor', theme.backgroundColor)
                   .set('borderColor', theme.borderColor)
