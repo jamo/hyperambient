a0496106c08b3462cf2b346f9845bd5e7c45271c
Jarmo Isotalo <jamo@isotalo.fi>
2016-07-21 23:50:48 +0300

    Code style for preinstall.json

--- preinstall.js
+++ preinstall.js
@@ -1,21 +1,21 @@
 const { homedir } = require('os')
 const { readFileSync, writeFileSync, accessSync } = require('fs')
-const { resolve } = require('path');
+const { resolve } = require('path')
 
 const configfile = resolve(homedir(), '.hyperambient.json')
 
 try {
   accessSync((configfile))
 } catch (e) {
-  return
+  process.exit(0)
 }
 
 const ambientConfig = JSON.parse(readFileSync(configfile, 'utf-8'))
 
-const packageJson = require('./package');
-packageJson.dependencies[ambientConfig.config.lightTheme] = "latest"
-packageJson.dependencies[ambientConfig.config.darkTheme] = "latest"
+const packageJson = require('./package')
+packageJson.dependencies[ambientConfig.config.lightTheme] = 'latest'
+packageJson.dependencies[ambientConfig.config.darkTheme] = 'latest'
 
 console.log(packageJson.dependencies)
-writeFileSync('package.json', JSON.stringify(packageJson, null, 2) + '\n');
+writeFileSync('package.json', JSON.stringify(packageJson, null, 2) + '\n')
 
